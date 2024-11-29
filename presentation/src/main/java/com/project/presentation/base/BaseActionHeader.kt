18df4a4d6a9ed7c2675a91ef513aae2b7cadd9c8
niaak <57080476+YuhoLee@users.noreply.github.com>
2024-11-29 14:55:16 +0900

    Feat: BaseActionHeader 추가

---
+++ presentation/src/main/java/com/project/presentation/base/BaseActionHeader.kt
@@ -0,0 +1,46 @@
+package com.project.presentation.base
+
+import android.content.Context
+import android.content.res.TypedArray
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.project.presentation.R
+import com.project.presentation.databinding.LayoutBaseActionHeaderBinding
+
+class BaseActionHeader: ConstraintLayout {
+    private lateinit var binding: LayoutBaseActionHeaderBinding
+
+    constructor(context: Context): super(context){
+        initView()
+    }
+
+    constructor(context: Context, attrs: AttributeSet): super(context, attrs){
+        initView()
+        getAttrs(attrs)
+    }
+
+    private fun initView(){
+        binding = LayoutBaseActionHeaderBinding.inflate(LayoutInflater.from(context), this, true)
+    }
+
+    private fun getAttrs(attrs: AttributeSet){
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.header)
+        setTypedArray(typedArray)
+        typedArray.recycle()
+    }
+
+    private fun setTypedArray(typedArray: TypedArray){
+        val titleStr = typedArray.getString(R.styleable.header_headerTitle)
+        val backEnabled = typedArray.getBoolean(R.styleable.header_backEnabled, false)
+        val favoriteEnabled = typedArray.getBoolean(R.styleable.header_favoriteEnabled, false)
+
+        binding.apply{
+            tvActionHeaderTitle.text = titleStr ?: ""
+            ivBack.visibility = if(backEnabled) View.VISIBLE else View.INVISIBLE
+            ivFavorite.visibility = if(favoriteEnabled) View.VISIBLE else View.INVISIBLE
+        }
+
+    }
+}
