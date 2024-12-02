ea2d36232d7fc350e6dc30c0517955d39cef2468
niaak <57080476+YuhoLee@users.noreply.github.com>
2024-12-02 18:20:15 +0900

    Feat: 홈화면과 즐겨찾기 화면에서 뒤로가기 물리 버튼 두 번 누르면 앱 종료 되는 기능 추가

--- presentation/src/main/java/com/project/presentation/main/HomeFragment.kt
+++ presentation/src/main/java/com/project/presentation/main/HomeFragment.kt
@@ -1,34 +1,51 @@
 package com.project.presentation.main
 
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
+import androidx.lifecycle.lifecycleScope
 import com.project.presentation.R
 import com.project.presentation.base.BaseFragment
 import com.project.presentation.databinding.FragmentHomeBinding
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
-class HomeFragment: BaseFragment<FragmentHomeBinding>(
+class HomeFragment : BaseFragment<FragmentHomeBinding>(
     bindingInflater = FragmentHomeBinding::inflate,
 ) {
-    override fun init() {
-        super.init()
-
-    }
-
-    override fun initView() {
-        super.initView()
-
-    }
+    private var backPressJob: Job? = null
 
     override fun initListener() {
         super.initListener()
 
-        binding.apply{
+        binding.apply {
             clSearchCollections.setOnClickListener {
                 navController.navigate(R.id.action_home_to_search)
             }
         }
+
+        // 뒤로가기 콜백 등록
+        requireActivity().onBackPressedDispatcher.addCallback(
+            this,
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    if (backPressJob?.isActive == true) {
+                        // 두 번째 뒤로가기: 앱 종료
+                        requireActivity().finish()
+                    } else {
+                        // 첫 번째 뒤로가기: 코루틴 실행
+                        backPressJob = lifecycleScope.launch {
+                            Toast.makeText(
+                                requireContext(),
+                                requireContext().getString(R.string.toast_double_back_text),
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            delay(2000) // 2초 대기
+                            backPressJob = null // 상태 초기화
+                        }
+                    }
+                }
+            })
     }
 
-    override fun observeViewModel() {
-        super.observeViewModel()
-
-    }
 }
